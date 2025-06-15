68497ff2c030dc408f91431731017a76a3063bb4
Stefodan21 <stefaandavis21@outlook.com>
2025-06-15 15:06:00 -0400

    Added github workflows and updated astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
+  site: "https://Stefodan21.github.io",
+  base: "/Stefodan21.github.io/",
   integrations: [preact(), 
     icon({
     iconDir: "./src/icons"
